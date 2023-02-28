2edb2862e019a2e3bd8f5dfd6a9b34999798cfe7
FulllMental <104234625+FulllMental@users.noreply.github.com>
2023-02-28 21:16:25 +0300

    Add frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'where_to_go.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
+
 def show_index(request):
     return render(request, 'index.html')
