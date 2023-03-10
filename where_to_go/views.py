9f4ec1fe37f0245f7790a71b0bb4a299fb509bba
Arsenii Mozharov <104234625+FulllMental@users.noreply.github.com>
2023-03-11 02:07:12 +0300

    Move data from db to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -4,36 +4,28 @@ from places.models import Place
 
 def show_index(request):
     all_places = Place.objects.all()
+    place_position = []
+    for place in all_places:
+        place_position.append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.latitude, place.longitude]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.pk,
+                    "detailsUrl": "static/places/moscow_legends.json"
+                }
+            }
+        )
+
     context = {
-      "places_geojson": {
-        "type": "FeatureCollection",
-        "features": [
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [37.62, 55.793676]
-            },
-            "properties": {
-              "title": "«Легенды Москвы",
-              "placeId": "moscow_legends",
-              "detailsUrl": "static/places/moscow_legends.json"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [37.64, 55.753676]
-            },
-            "properties": {
-              "title": "Крыши24.рф",
-              "placeId": "roofs24",
-              "detailsUrl": "static/places/roofs24.json"
-            }
-          }
-        ]
-      }
+        'places_geojson': {
+            "type": "FeatureCollection",
+            "features": place_position
+        }
     }
 
     return render(request, 'index.html', context)
